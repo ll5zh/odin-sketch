ba784afcf6ccb288277ae6e3e30430428b40cf17
Lucia <122565245+ll5zh@users.noreply.github.com>
2023-06-27 16:10:27 -0400

    Add sketch-on-hover functionality for single colour and grid size

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ function makeRow(num) {
     row = document.createElement('div');
     row.classList.add('row');
     for (let i = 0; i < num; ++i) {
-        cell = document.createElement('div');
+        let cell = document.createElement('div');
+        // ^^ who knew! adding "let" made the colour change every time because then each cell would be a separate variable LOL
         cell.classList.add('cell');
         height = getCellHeight(16);
         cell.style.height = height + 'px';
         thickness = 1 / num;
         cell.style.border = thickness + 'px black solid';
+        cell.addEventListener('mouseover', function(e) {
+            cell.style.cssText = 'background-color: lightcoral';
+            height = getCellHeight(16);
+            cell.style.height = height + 'px';
+            thickness = 1 / num;
+            cell.style.border = thickness + 'px black solid';
+        }); // colour only changes for one of them
         row.appendChild(cell);
     }
     container.appendChild(row);
@@ -23,6 +31,10 @@ function makeGrid(num) {
 
 function getCellHeight(num) {
     return 600 / num;
+}
+
+function colorChange(e) {
+    e.style.cssText = 'background-color: red';
 }
 
 makeGrid(16);
