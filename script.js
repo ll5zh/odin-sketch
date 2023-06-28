3ac96a5adf9c648f4a5d535617db4781fece4bf0
Lucia <122565245+ll5zh@users.noreply.github.com>
2023-06-28 10:34:49 -0400

    Add ability to change grid size, update interface

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('#container');
+let gridSize = 16;
 
 function makeRow(num) {
     row = document.createElement('div');
@@ -7,15 +8,15 @@ function makeRow(num) {
         let cell = document.createElement('div');
         // ^^ who knew! adding "let" made the colour change every time because then each cell would be a separate variable LOL
         cell.classList.add('cell');
-        height = getCellHeight(16);
+        height = getCellHeight(num);
         cell.style.height = height + 'px';
-        thickness = 1 / num;
+        thickness = 3 / num;
         cell.style.border = thickness + 'px black solid';
         cell.addEventListener('mouseover', function(e) {
             cell.style.cssText = 'background-color: lightcoral';
-            height = getCellHeight(16);
+            height = getCellHeight(num);
             cell.style.height = height + 'px';
-            thickness = 1 / num;
+            thickness = 3 / num;
             cell.style.border = thickness + 'px black solid';
         }); // colour only changes for one of them
         row.appendChild(cell);
@@ -30,13 +31,38 @@ function makeGrid(num) {
 }
 
 function getCellHeight(num) {
-    return 600 / num;
+    return 600 / gridSize;
 }
 
 function colorChange(e) {
     e.style.cssText = 'background-color: red';
 }
 
-makeGrid(16);
+const btn = document.querySelector('#newgrid');
+const print = document.querySelector('#size');
 
-console.log(container);
+btn.addEventListener('click', function(e) {
+    console.log(e);
+    let size = prompt("How wide is your grid? Enter a number between 1 and 100!");
+    if (1 <= size && size <= 100) {
+        gridSize = size;
+        console.log(gridSize);
+        container.innerHTML=''; // clears what was originally in container
+        makeGrid(gridSize);
+        // location.reload();
+        // goal: either refresh page with new gridSize and grid, or change grid
+        // ^^ refresh probably doesn't work because default gridSize is 16 for sure
+        // ^^ remove current grid, remake
+        print.innerHTML = 'Current Grid Size: ' + gridSize + ' x ' + gridSize;
+    } else {
+        alert('Sorry, your response was invalid.')
+    }
+});
+
+const refresh = document.querySelector('#btn');
+console.log(refresh);
+refresh.addEventListener('click', function(e) {
+    location.reload();
+});
+
+makeGrid(gridSize);
